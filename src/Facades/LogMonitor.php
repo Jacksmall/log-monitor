7ed34d3e89af794826947f0d80ff3e38344ad76b
chenkuan <812254315@qq.com>
2025-05-23 18:24:31 +0800

    2

--- src/Facades/LogMonitor.php
+++ src/Facades/LogMonitor.php
@@ -3,11 +3,12 @@
 namespace Jack\LogMonitor\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jack\LogMonitor\AbstractLogFactory;
 
 class LogMonitor extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'logmonitor';
+        return AbstractLogFactory::class;
     }
 }
