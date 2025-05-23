f487fb11c656266ce78a1462963ff4fc8f3afbbb
chenkuan <812254315@qq.com>
2025-05-23 19:18:32 +0800

    2

--- src/AbstractLogFactory.php
+++ src/AbstractLogFactory.php
@@ -2,10 +2,41 @@
 
 namespace Jack\LogMonitor;
 
+use Exception;
 use Jack\LogMonitor\Services\LogInterface;
 
 abstract class AbstractLogFactory
 {
+    private $data = [];
+
+    public function __construct()
+    {
+        $this->initData();
+    }
+
+    /**
+     * @return void
+     */
+    protected function initData()
+    {
+        $this->data = [
+            'timestamp' => microtime(true),
+            'host'      => gethostname(),
+            'env'       => app()->environment()
+        ];
+    }
+
+    public function set($name, $value)
+    {
+        $this->data[$name] = $value;
+        return $this;
+    }
+
+    public function get($name, $default = null)
+    {
+        return $this->data[$name] ?? $default;
+    }
+
     /**
      * @return LogInterface
      */
@@ -17,12 +48,20 @@ abstract class AbstractLogFactory
      * @param $level
      * @param $message
      * @param array $context
-     * @param array $data
      * @return mixed
      */
-    public function log($level, $message, array $context = [], array $data = [])
+    public function log($level, $message, array $context = [])
     {
         $log = $this->createLogger();
-        return $log->log($level, $message, $context, $data);
+        return $log->log($level, $message, $context, $this->data);
+    }
+
+    public function catchException(Exception $exception)
+    {
+        $this->log('ERROR', $exception->getMessage(), [
+            'file'  => $exception->getFile(),
+            'line'  => $exception->getLine(),
+            'trace' => $exception->getTraceAsString()
+        ]);
     }
 }
