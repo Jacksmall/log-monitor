b4d1621f23b25b4f78ba6725c427962694e79435
chenkuan <>
2025-06-05 17:34:17 +0800

    redis log factory

--- src/Facades/LogMonitor.php
+++ src/Facades/LogMonitor.php
@@ -3,12 +3,12 @@
 namespace Jack\LogMonitor\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Jack\LogMonitor\Factory\RedisLogFactory;
+use Jack\LogMonitor\AbstractLogFactory;
 
 class LogMonitor extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return RedisLogFactory::class;
+        return AbstractLogFactory::class;
     }
 }
--- src/LogMonitorServiceProvider.php
+++ src/LogMonitorServiceProvider.php
@@ -3,14 +3,13 @@
 namespace Jack\LogMonitor;
 
 use Illuminate\Support\ServiceProvider;
-use Jack\LogMonitor\Factory\RedisLogFactory;
 
 class LogMonitorServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/logmonitor.php', 'logmonitor');
-        $this->app->bind(RedisLogFactory::class, function ($app) {
+        $this->app->bind(AbstractLogFactory::class, function ($app) {
             return LogFactoryRouter::createFactory(config('logmonitor.default'));
         });
     }
